e7fb2da5b08fd500f10eeec59c7ed1c9ceb968a3
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-25 08:55:58 -0300

    requisito 4 acho que foi kek

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,14 +23,23 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+    expect(circle('testando ele')).toBe(undefined);
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(typeof circle(13)).toBe('object');
     // Teste se circle retorna um objeto.
+    expect(Object.keys(circle(13))).toHaveLength(3);
     // Teste se o objeto retornado possui 3 propriedades.
+    expect(circle()).toBe(undefined);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle(2).circumference).toBe(12.56)
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
+    expect(circle(3).area).toBeCloseTo(28.259);
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
+    expect(circle(3)).toEqual({ radius: 3, area: 28.259999999999998, circumference: 18.84});
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
   });
 });
+
+
+// Tive que descobrir como usar o toHaveLength encontrei nesse link https://jestjs.io/pt-BR/docs/expect#tohavelengthnumber

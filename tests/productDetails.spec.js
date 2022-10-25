c13e22b947a8071cdece7ad94e90c02b1e29cc50
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-25 09:23:39 -0300

    requisito 6 feito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
+    expect(typeof productDetails).toBe('function');
     // Teste se productDetails é uma função.
+    expect(productDetails()).toBeInstanceOf(Array);
     // Teste se o retorno da função é um array.
+    expect(productDetails()).toHaveLength(2);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(typeof productDetails('Álcool gel','Máscara')[0]).toBe('object');
+    expect(typeof productDetails('Álcool gel','Máscara')[1]).toBe('object');
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(productDetails('Álcool gel','Máscara')[0]).not.toBe(productDetails('Álcool gel','Máscara')[1]);
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('Álcooll gel','Máscara')[0].details.productId.endsWith('123')).toBe(true);
+    expect(productDetails('Álcooll gel','Máscara')[1].details.productId.endsWith('123')).toBe(true);
     // Teste se os dois productIds terminam com 123.
   });
 });
+
+
+// Encontrei sobre o tobeinstanceof no link https://jestjs.io/pt-BR/docs/expect#tohavelengthnumber

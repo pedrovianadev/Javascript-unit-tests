bc0408d9e826055217ff400995acf27deb925004
Pedro Viana <pedroviana.developer13@gmail.com>
2022-10-25 20:13:18 -0300

    requisito 8

--- src/average.js
+++ src/average.js
@@ -12,8 +12,9 @@
 */
 
 const average = (array) => {
-  if (array.length === 0) return undefined;
-
+  if (array.length === 0) {
+    return undefined;
+  }
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
